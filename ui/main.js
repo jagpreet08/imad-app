6bfc5ee1a66a6fc9ef6550755836909195824bc3
jagpreet08 <jagpreet08@gmail.com>
2017-08-07 10:04:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,21 @@ button.onclick = function() {
    request.send(null);
    
 };
+
+//submit name
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function () {
+    //make a request to the server & send the name
+    
+    
+    //captur a list of the names & render it as a list
+    var names = ['name1' , 'name2', 'name3', 'name4'];
+    var list = '' ;
+    for (var i=0; i<names.length; i++){
+        list +='<li>' + names[i] + '</li>'
+    }
+    var ul= docuemnt.getElementById('namelist');
+    ul.innerHTML=list;
+};
